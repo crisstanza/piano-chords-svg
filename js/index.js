723a3f6f89bcfe0a70e6ae0734df880018bd38d4
crisstanza <crisstanza@home>
2018-09-29 20:39:34 -0300

    Início da manipulação dos eventos e interação com o usuário.
    Carregando Cm por default.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,34 @@
 (function() {
 
+	var linksChords, links;
 	var svg;
 
 // // // // // // // // // // // // // // // // // // // // // // // // //
 
-	function x(evento) {
+	function x(event) {
 	}
 
 // // // // // // // // // // // // // // // // // // // // // // // // //
 
-	function initGlobals() {
+	function initGlobals(event) {
+		linksChords = Utils.$('links-chords');
+		links = linksChords.getElementsByTagName('a');
 		svg = Utils.$('svg');
 	}
 
-	function window_Load() {
-		initGlobals();
+	function initHash(event) {
+		if (!document.location.hash) {
+			document.location.hash = 'Cm';
+		}
 	}
 
-	function window_HashChange() {
+	function window_Load(event) {
+		initGlobals(event);
+		initHash(event);
+	}
+
+	function window_HashChange(event) {
+		console.log(links);
 	}
 
 	window.addEventListener('load', window_Load);
