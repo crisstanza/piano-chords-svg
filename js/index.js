0b1281fdef53678a3e90c7215b04e1d79d5857a2
crisstanza <crisstanza@home>
2018-09-30 12:57:19 -0300

    Troca de notas, menor e sustenido (wip).

--- js/index.js
+++ js/index.js
@@ -1,35 +1,77 @@
 (function() {
 
-	var linksChords, links;
+	var linksChords, links, linksMap, minor, sharp, chordName;
 	var svg;
 
 // // // // // // // // // // // // // // // // // // // // // // // // //
 
-	function x(event) {
+	function loadChord(hash) {
+		var chord = hash.substring(1);
+		chordName.innerText = chord;
+		//
+		for (var i = 0 ; i < links.length ; i++) {
+			var link = links[i];
+			link.setAttribute('class', '');
+		}
+		var root = chord.substring(0, 1);
+		linksMap[root].setAttribute('class', 'current');
 	}
 
 // // // // // // // // // // // // // // // // // // // // // // // // //
 
+	function initChecks(event) {
+		minor.addEventListener('click', modifiers_Click)
+		sharp.addEventListener('click', modifiers_Click)
+	}
+
 	function initGlobals(event) {
 		linksChords = Utils.$('links-chords');
 		links = linksChords.getElementsByTagName('a');
+		linksMap = [];
+		for (var i = 0 ; i < links.length ; i++) {
+			var link = links[i];
+			linksMap[link.innerText] = link;
+		}
+		minor = Utils.$('minor');
+		sharp = Utils.$('sharp');
+		chordName = Utils.$('chord-name');
 		svg = Utils.$('svg');
 	}
 
 	function initHash(event) {
-		if (!document.location.hash) {
+		var hash = document.location.hash;
+		if (hash) {
+			loadChord(hash);
+		} else {
 			document.location.hash = 'Cm';
+		}
+	}
+
+// // // // // // // // // // // // // // // // // // // // // // // // //
+
+	function modifiers_Click(event) {
+		var isMinor = minor.checked;
+		var isSharp = sharp.checked;
+		var modifiers = isMinor ? 'm' : '';
+		modifiers += isSharp ? '#' : '';
+		for (var i = 0 ; i < links.length ; i++) {
+			var link = links[i];
+			link.href = '#' + link.innerText + modifiers;
 		}
 	}
 
 	function window_Load(event) {
 		initGlobals(event);
+		initChecks(event);
 		initHash(event);
 	}
 
 	function window_HashChange(event) {
-		console.log(links);
+		var hash = document.location.hash;
+		loadChord(hash);
 	}
+
+// // // // // // // // // // // // // // // // // // // // // // // // //
 
 	window.addEventListener('load', window_Load);
 	window.addEventListener('hashchange', window_HashChange);
