416ef0e0fb2d52ff71b4af4e80aa4504975f19e6
crisstanza <crisstanza@home>
2018-12-04 22:27:50 -0200

    U2 - I Still Haven't Found What I'm Looking For / metrônomo

--- js/music.js
+++ js/music.js
@@ -1,6 +1,7 @@
 (function() {
 
 	var parent, musicPre, musicLinks;
+	var metronome;
 
 // // // // // // // // // // // // // // // // // // // // // // // // //
 
@@ -8,6 +9,91 @@
 		parent = window.opener ? window.opener.parent : null;
 		musicPre = Utils.$('music-pre');
 		musicLinks = musicPre.querySelectorAll('a');
+		metronome = {
+			beat: null,
+			audioContext: null,
+			SOUND_1: null,
+			SOUND_4: null,
+			display: Utils.$('metronome-display'),
+			btUspeedUp: Utils.$('bt-speed-up'),
+			btSpeedDown: Utils.$('bt-speed-down'),
+			btPlay: Utils.$('bt-play'),
+			btPlay_Click: function(event) {
+				var bt = event.target;
+				if (bt.getAttribute('class') == 'arrow-right') {
+					bt.setAttribute('class', 'arrow-right-square');
+					metronome.start();
+				} else {
+					bt.setAttribute('class', 'arrow-right');
+					metronome.stop();
+				}
+			},
+			btSpeedUp_Click: function(event) {
+				metronome.display.innerText++;
+			},
+			btSpeedDown_Click: function(event) {
+				metronome.display.innerText--;
+			},
+			start: function(event) {
+				metronome.beat = 0;
+				metronome.play();
+				METRONOME = setInterval(
+					function() {
+						metronome.play();
+					}, (60000 / metronome.display.innerText)/2
+				);
+			},
+			stop: function(event) {
+				clearInterval(METRONOME);
+			},
+			play: function() {
+				var bufferSource = metronome.audioContext.createBufferSource();
+				bufferSource.buffer = null
+				if (metronome.beat <= 7) {
+					if (metronome.beat % 2 == 0) {
+						bufferSource.buffer = metronome.SOUND_4;
+					}
+				} else if (metronome.beat % 4 == 0) {
+					bufferSource.buffer = metronome.SOUND_1;
+				} else if ((metronome.beat + 2) % 4 == 0) {
+					bufferSource.buffer = metronome.SOUND_4;
+				} else if (metronome.beat == 23) {
+					bufferSource.buffer = metronome.SOUND_4;
+				} else if (metronome.beat == 55) {
+					bufferSource.buffer = metronome.SOUND_4;
+				} else {
+				}
+				if (bufferSource.buffer != null) {
+					bufferSource.connect(metronome.audioContext.destination);
+					bufferSource.start();
+				}
+	    	    metronome.beat++;
+			},
+			init: function(event) {
+				metronome.audioContext = new AudioContext() || new webkitAudioContext();
+				function onDecoded_1(buffer) {
+					metronome.SOUND_1 = buffer;
+				}
+				function onDecoded_4(buffer) {
+					metronome.SOUND_4 = buffer;
+				}
+				var request_1 = new XMLHttpRequest();
+				request_1.open('GET', './audio/_1.mp3', true);
+				request_1.responseType = 'arraybuffer';
+				request_1.onload = function() {
+					metronome.audioContext.decodeAudioData(request_1.response, onDecoded_1);
+				};
+				request_1.send();
+				var request_4 = new XMLHttpRequest();
+				request_4.open('GET', './audio/_4.mp3', true);
+				request_4.responseType = 'arraybuffer';
+				request_4.onload = function() {
+					metronome.audioContext.decodeAudioData(request_4.response, onDecoded_4);
+				};
+				request_4.send();
+			}
+		};
+		metronome.init(event);
 	}
 
 	function initLinks(event) {
@@ -15,6 +101,9 @@
 			var musicLink = musicLinks[i];
 			musicLink.addEventListener('click', link_Click);
 		}
+		metronome.btPlay.addEventListener('click', metronome.btPlay_Click);
+		metronome.btUspeedUp.addEventListener('click', metronome.btSpeedUp_Click);
+		metronome.btSpeedDown.addEventListener('click', metronome.btSpeedDown_Click);
 	}
 
 // // // // // // // // // // // // // // // // // // // // // // // // //
