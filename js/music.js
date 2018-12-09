ccf3fe637335e086ffce11c195b6a3becce55c4a
crisstanza <crisstanza@home>
2018-12-09 13:10:50 -0200

    Batidas do metrônomo / marcações bateria.

--- js/music.js
+++ js/music.js
@@ -12,10 +12,11 @@
 		metronome = {
 			beat: null,
 			beatNotes: null,
-			beatNote: null,
 			audioContext: null,
 			SOUNDS: null,
+			TIMER: null,
 			display: Utils.$('metronome-display'),
+			displayCurrentBeat: Utils.$('metronome-display-current-beat'),
 			btUspeedUp: Utils.$('bt-speed-up'),
 			btSpeedDown: Utils.$('bt-speed-down'),
 			btPlay: Utils.$('bt-play'),
@@ -23,10 +24,10 @@
 				var bt = event.target;
 				if (bt.getAttribute('class') == 'arrow-right') {
 					bt.setAttribute('class', 'arrow-right-square');
-					metronome.start();
+					metronome.start(event);
 				} else {
 					bt.setAttribute('class', 'arrow-right');
-					metronome.stop();
+					metronome.stop(event);
 				}
 			},
 			btSpeedUp_Click: function(event) {
@@ -37,19 +38,28 @@
 			},
 			start: function(event) {
 				var MUL = 2;
-				metronome.beat = -4 * MUL;
-				metronome.beatNote = 0;
-				metronome.play();
-				METRONOME = setInterval(
+				if (metronome.displayCurrentBeat.value == '') {
+						metronome.beat = -4 * MUL;
+				} else {
+					var currentBeat = metronome.displayCurrentBeat.value * 1;
+					if (currentBeat >= metronome.beatNotes.length) {
+						metronome.beat = -4 * MUL;
+					} else {
+						metronome.beat = metronome.displayCurrentBeat.value * 1;
+					}
+				}
+				metronome.play(event);
+				metronome.TIMER = setInterval(
 					function() {
-						metronome.play();
+						metronome.play(event);
 					}, 60000 / (metronome.display.innerText * MUL)
 				);
 			},
 			stop: function(event) {
-				clearInterval(METRONOME);
+				clearInterval(metronome.TIMER);
 			},
-			play: function() {
+			play: function(event) {
+				metronome.displayCurrentBeat.value = metronome.beat;
 				var bufferSource = metronome.audioContext.createBufferSource();
 				bufferSource.buffer = null;
 				if (metronome.beat < 0) {
@@ -64,7 +74,7 @@
 							bufferSource.buffer = metronome.SOUNDS[note*1];
 						}
 					} else {
-						metronome.btPlay_Click({target: Utils.$('bt-play')});
+						metronome.btPlay_Click(event);
 					}
 				}
 				if (bufferSource.buffer != null) {
@@ -102,6 +112,7 @@
 		for (var i = 0 ; i < musicLinks.length ; i++) {
 			var musicLink = musicLinks[i];
 			musicLink.addEventListener('click', link_Click);
+			musicLink.addEventListener('dblclick', link_DoubleClick);
 		}
 		metronome.btPlay.addEventListener('click', metronome.btPlay_Click);
 		metronome.btUspeedUp.addEventListener('click', metronome.btSpeedUp_Click);
@@ -111,9 +122,21 @@
 // // // // // // // // // // // // // // // // // // // // // // // // //
 
 	function link_Click(event) {
+		event.preventDefault();
 		var link = event.target
 		if (parent) {
 			parent.location.hash = link.innerText;
+		}
+	}
+
+	function link_DoubleClick(event) {
+		var link = event.target
+		var beatNote = link.getAttribute('title');
+		if (metronome) {
+			if (beatNote != '') {
+				metronome.beat = beatNote;
+				metronome.displayCurrentBeat.value = beatNote;
+			}
 		}
 	}
 
